3403996de8d1a40d03794d6bbc2c96fa1ffc6fb1
rodolfomori <rod_dolpho@hotmail.com>
2023-01-08 19:18:23 -0300

    add new shadow button

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-let sec = 15
-const startCounter = () => {
-    setInterval(counter, 1000)
-}
+// let sec = 15
+// const startCounter = () => {
+//     setInterval(counter, 1000)
+// }
 
-const counter = () => {
-    if(sec >= 0 ){
-        document.getElementById('timer').innerText = sec--     
-    }
+// const counter = () => {
+//     if(sec >= 0 ){
+//         document.getElementById('timer').innerText = sec--     
+//     }
     
-    if(sec == 0 ){
-        setTimeout(()=>{
-             window.location.href = 'https://rodolfomori.com.br/grupovip'
-        },1500)
-    }
-}
+//     if(sec == 0 ){
+//         setTimeout(()=>{
+//              window.location.href = 'https://rodolfomori.com.br/grupovip'
+//         },1500)
+//     }
+// }
 
- startCounter()
-a
+//  startCounter()
+// a
